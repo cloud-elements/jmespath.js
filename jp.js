b3213a1d9edb3845e73b1d0fd0cb84b91b881f6d
Rocky Madden <git@rockymadden.com>
2020-03-06 09:43:09 -0700

    update to 2015

--- jp.js
+++ jp.js
@@ -1,23 +1,25 @@
 #!/usr/bin/env node
-jmespath = require('./jmespath');
+jmespath = require('./src/jmespath');
 
 process.stdin.setEncoding('utf-8');
 
-
 if (process.argv.length < 2) {
-    console.log("Must provide a jmespath expression.");
+    console.error("Must provide a JMESPath expression.");
     process.exit(1);
 }
-var inputJSON = "";
+
+let inputJSON = "";
 
 process.stdin.on('readable', function() {
-    var chunk = process.stdin.read();
+    let chunk = process.stdin.read();
+
     if (chunk !== null) {
         inputJSON += chunk;
     }
 });
 
 process.stdin.on('end', function() {
-    var parsedInput = JSON.parse(inputJSON);
+    let parsedInput = JSON.parse(inputJSON);
+
     console.log(JSON.stringify(jmespath.search(parsedInput, process.argv[2])));
 });
