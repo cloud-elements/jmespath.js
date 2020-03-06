d223dba540384bf50a593cc1ab0469edd960f82e
Rocky Madden <git@rockymadden.com>
2020-03-06 09:51:24 -0700

    two spaces not four

--- bin/jmespath.js
+++ bin/jmespath.js
@@ -4,22 +4,22 @@ jmespath = require('../src/jmespath');
 process.stdin.setEncoding('utf-8');
 
 if (process.argv.length < 2) {
-    console.error("Must provide a JMESPath expression.");
-    process.exit(1);
+  console.error("Must provide a JMESPath expression.");
+  process.exit(1);
 }
 
 let inputJSON = "";
 
 process.stdin.on('readable', function() {
-    let chunk = process.stdin.read();
+  let chunk = process.stdin.read();
 
-    if (chunk !== null) {
-        inputJSON += chunk;
-    }
+  if (chunk !== null) {
+    inputJSON += chunk;
+  }
 });
 
 process.stdin.on('end', function() {
-    let parsedInput = JSON.parse(inputJSON);
+  let parsedInput = JSON.parse(inputJSON);
 
-    console.log(JSON.stringify(jmespath.search(parsedInput, process.argv[2])));
+  console.log(JSON.stringify(jmespath.search(parsedInput, process.argv[2])));
 });
